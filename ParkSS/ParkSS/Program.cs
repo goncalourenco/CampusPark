6bb023c0e2d3fe55b29ef2c0f25ae8bfa2d06967
Gonçalo Lourenço <goncalo.blourenc@gmail.com>
2018-12-06 19:11:29 +0000

    ParkSS final updates

--- ParkSS/ParkSS/Program.cs
+++ ParkSS/ParkSS/Program.cs
@@ -47,67 +47,97 @@ namespace ParkSS
                 case "spots":
                     Console.WriteLine($"{e.Topic} : {Encoding.UTF8.GetString(e.Message)}" + Environment.NewLine);
                     ParkingSpot spot = DeserializeParkingSpot(Encoding.UTF8.GetString(e.Message));
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        connection.Open();
-                        SqlCommand cmd = new SqlCommand("SELECT * FROM ParkingSpots WHERE Name=@name", connection);
-                        cmd.Parameters.AddWithValue("@name", spot.Name);
-                        cmd.ExecuteNonQuery();
-                        SqlDataReader reader = cmd.ExecuteReader();
-                        bool hasRows = reader.HasRows;
-                        reader.Close();
-                        if (!hasRows)
-                        {
-                            cmd = new SqlCommand("INSERT INTO ParkingSpots(Name, Id, Location, BatteryStatus, Timestamp, Value) VALUES(@name, @id, @location, @batteryStatus, @timestamp, @value)", connection);
-                            cmd.Parameters.AddWithValue("@id", spot.Id);
-                            cmd.Parameters.AddWithValue("@location", spot.Location);
-                            cmd.Parameters.AddWithValue("@batteryStatus", spot.BatteryStatus);
-                            cmd.Parameters.AddWithValue("@timestamp", spot.Status.Timestamp);
-                            cmd.Parameters.AddWithValue("@value", spot.Status.Value);
-                            cmd.Parameters.AddWithValue("@name", spot.Name);
-                            cmd.ExecuteNonQuery();
-                        }
-                        else
-                        {
-                            cmd = new SqlCommand("UPDATE ParkingSpots SET BatteryStatus=@batteryStatus, Timestamp=@timestamp, Value=@value WHERE Name=@name", connection);               
-                            cmd.Parameters.AddWithValue("@batteryStatus", spot.BatteryStatus);
-                            cmd.Parameters.AddWithValue("@timestamp", spot.Status.Timestamp);
-                            cmd.Parameters.AddWithValue("@value", spot.Status.Value);
-                            cmd.Parameters.AddWithValue("@name", spot.Name);
-                            cmd.ExecuteNonQuery();
-                        }
-                        connection.Close();
-                    }
+                    InsertOrUpdateSpotsTable(spot);
                     break;
                 case "parksInfo":
                     Console.WriteLine($"{e.Topic} : {Encoding.UTF8.GetString(e.Message)} INFO" + Environment.NewLine);
                     var parkList = DeserializeParks(Encoding.UTF8.GetString(e.Message));
-                    foreach (Park park in parkList)
-                    {
-                        using (SqlConnection connection = new SqlConnection(connectionString))
-                        {
-                            connection.Open();
-                            SqlCommand cmd = new SqlCommand("SELECT * FROM Parks WHERE id=@id", connection);
-                            cmd.Parameters.AddWithValue("@id", park.Id);
-                            cmd.ExecuteNonQuery();
-                            SqlDataReader reader = cmd.ExecuteReader();
-                            bool hasRows = reader.HasRows;
-                            reader.Close();
-                            if (!hasRows)
-                            {
-                                cmd = new SqlCommand("INSERT INTO Parks(Id, Description, NumberOfSpecialSpots, OperatingHours, GeoLocationFile) VALUES(@id, @description, @numberOfSpecialSpots, @operatingHours, @geoLocationFile)", connection);
-                                cmd.Parameters.AddWithValue("@id", park.Id);
-                                cmd.Parameters.AddWithValue("@description", park.Description);
-                                cmd.Parameters.AddWithValue("@numberOfSpecialSpots", park.NumberOfSpecialSpots);
-                                cmd.Parameters.AddWithValue("@operatingHours", park.OperatingHours);
-                                cmd.Parameters.AddWithValue("@geoLocationFile", park.GeoLocationFile);
-                                cmd.ExecuteNonQuery();
-                            }
-                            
-                            connection.Close();
-                        }
-                    }
+                    InsertParksTable(parkList);
                     break;
+            }
+        }
+
+        private void InsertParksTable(List<Park> parkList)
+        {
+            foreach (Park park in parkList)
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Parks WHERE id=@id", connection);
+                    cmd.Parameters.AddWithValue("@id", park.Id);
+                    cmd.ExecuteNonQuery();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    bool hasRows = reader.HasRows;
+                    reader.Close();
+                    if (!hasRows)
+                    {
+                        cmd = new SqlCommand("INSERT INTO Parks(Id, Description, NumberOfSpecialSpots, OperatingHours, GeoLocationFile, NumberOfSpots) VALUES(@id, @description, @numberOfSpecialSpots, @operatingHours, @geoLocationFile, @numberOfSpots)", connection);
+                        cmd.Parameters.AddWithValue("@id", park.Id);
+                        cmd.Parameters.AddWithValue("@description", park.Description);
+                        cmd.Parameters.AddWithValue("@numberOfSpecialSpots", park.NumberOfSpecialSpots);
+                        cmd.Parameters.AddWithValue("@operatingHours", park.OperatingHours);
+                        cmd.Parameters.AddWithValue("@geoLocationFile", park.GeoLocationFile);
+                        cmd.Parameters.AddWithValue("@numberOfSpots", park.NumberOfSpecialSpots);
+                        cmd.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        private void InsertOrUpdateSpotsTable(ParkingSpot spot)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM ParkingSpots WHERE Name=@name", connection);
+                cmd.Parameters.AddWithValue("@name", spot.Name);
+                cmd.ExecuteNonQuery();
+                SqlDataReader reader = cmd.ExecuteReader();
+                bool hasRows = reader.HasRows;
+                reader.Close();
+                if (!hasRows)
+                {
+                    cmd = new SqlCommand("INSERT INTO ParkingSpots(Name, Id, Location, BatteryStatus, Timestamp, Value) VALUES(@name, @id, @location, @batteryStatus, @timestamp, @value)", connection);
+                    cmd.Parameters.AddWithValue("@id", spot.Id);
+                    cmd.Parameters.AddWithValue("@location", spot.Location);
+                    cmd.Parameters.AddWithValue("@batteryStatus", spot.BatteryStatus);
+                    cmd.Parameters.AddWithValue("@timestamp", spot.Status.Timestamp);
+                    cmd.Parameters.AddWithValue("@value", spot.Status.Value);
+                    cmd.Parameters.AddWithValue("@name", spot.Name);
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    /**
+                     * Se o campo alterado for value, copiar linha para o historico e só depois fazer update 
+                     * */
+                    cmd = new SqlCommand("SELECT Name, Timestamp, Value FROM ParkingSpots WHERE Name=@name", connection);
+                    cmd.Parameters.AddWithValue("@name", spot.Name);
+                    cmd.ExecuteNonQuery();
+                    SqlDataReader valueFromDB = cmd.ExecuteReader();
+                    valueFromDB.Read();
+                    string value = valueFromDB.GetString(2), timestamp = valueFromDB.GetString(1), name = valueFromDB.GetString(0);
+                    valueFromDB.Close();
+
+                    if (value != spot.Status.Value)
+                    {
+                        cmd = new SqlCommand("INSERT INTO SpotsHistory(Name, Timestamp, Value) VALUES(@name, @timestamp, @value)", connection);
+                        cmd.Parameters.AddWithValue("@timestamp", timestamp);
+                        cmd.Parameters.AddWithValue("@value", value);
+                        cmd.Parameters.AddWithValue("@name", name);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    cmd = new SqlCommand("UPDATE ParkingSpots SET BatteryStatus=@batteryStatus, Timestamp=@timestamp, Value=@value WHERE Name=@name", connection);
+                    cmd.Parameters.AddWithValue("@batteryStatus", spot.BatteryStatus);
+                    cmd.Parameters.AddWithValue("@timestamp", spot.Status.Timestamp);
+                    cmd.Parameters.AddWithValue("@value", spot.Status.Value);
+                    cmd.Parameters.AddWithValue("@name", spot.Name);
+                    cmd.ExecuteNonQuery();
+                }
+                connection.Close();
             }
         }
 
